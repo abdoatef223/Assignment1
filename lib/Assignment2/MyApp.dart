5cfe1bb0936b5521f982c41de38b75cddc286a22
Abdelrahman Atef <99149885+abdoatef223@users.noreply.github.com>
2026-06-22 19:37:44 +0300

    Delete lib/Assignment2/MyApp.dart

--- lib/Assignment2/MyApp.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutterprojects/Assignment1/Home_Screen.dart';
-
-class Myapp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: HomeScreen());
-  }
-}
